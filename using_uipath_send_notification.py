6050c6f31a199667695a73e0ed45a98339eeb727
Shreyank Srinivasan Harisha <131869365+Shreyank-SH@users.noreply.github.com>
2025-11-06 21:36:22 +0530

    Add files via upload

---
+++ using_uipath_send_notification.py
@@ -0,0 +1,19 @@
+import subprocess
+def send_notification(number):
+    uipath_exe = r"C:\Program Files\UiPath\Studio\UiRobot.exe"
+    uipath_pkg = r"C:\Users\Shreyank_SH\Documents\UiPath\Published\sendsms.1.0.4.nupkg"
+
+    recipient = "+917899374579"
+    json_input = f'{{"phoneNumber": "{recipient}"}}'  # ✅ Proper JSON
+
+
+    result = subprocess.run([
+        uipath_exe,
+        "execute",
+        "--file", uipath_pkg,
+        "--input", json_input
+    ], capture_output=True, text=True)
+
+    print("Return code:", result.returncode)
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
